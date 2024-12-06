a396d758c289043b43f6a852e7c72ec87889dfee
ethanwu1993 <ethanwu93@outlook.com>
2024-12-06 11:06:43 -0600

    update

---
+++ course/dbtlearn/models/src/src_reviews.sql
@@ -0,0 +1,13 @@
+WITH raw_reviews AS (
+    SELECT
+        *
+    FROM
+        {{source('airbnb', 'reviews')}}
+)
+SELECT
+    LISTING_ID,
+    DATE as review_date,
+    REVIEWER_NAME,
+    COMMENTS as review_text,
+    SENTIMENT as review_sentiment
+FROM raw_reviews
